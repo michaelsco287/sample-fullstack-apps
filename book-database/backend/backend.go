efe73fef408ed4a6fa533267fda00757095086b8
Nickson Thanda <nicksonthanda10@msn.com>
2020-02-10 11:32:35 +0000

    return bad request when invalid ISBN provided

--- book-database/backend/backend.go
+++ book-database/backend/backend.go
@@ -11,7 +11,11 @@ func setupRouter() *gin.Engine {
 	r.POST("/ownedbooks", func(c *gin.Context) {
 		lib := Library{}
 		isbn := c.PostForm("isbn")
-		foundBook, _ := FindBookByISBN(isbn)
+		foundBook, err := FindBookByISBN(isbn)
+		if err != nil {
+			c.String(400, fmt.Sprintf("Invalid ISBN (%v) provided.", isbn))
+			return
+		}
 		lib.OwnBook(foundBook)
 		c.String(200, fmt.Sprintf("Added \"%s\" to your list of owned books.", foundBook.title))
 	})
--- book-database/backend/backend_test.go
+++ book-database/backend/backend_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPostOwnedBooksRoute(t *testing.T) {
+func TestPostOwnedBooksRouteValidISBN(t *testing.T) {
 	mockResponse(http.StatusOK, map[string]string{"Content-Type": "application/json"}, []byte(`{
 		"ISBN:9781416590316":{
 		   "publishers":[
@@ -41,4 +41,17 @@ func TestPostOwnedBooksRoute(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "Added \"Frederick Douglass: Prophet of Freedom\" to your list of owned books.", w.Body.String())
+}
+func TestPostOwnedBooksRouteInvalidISBN(t *testing.T) {
+
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/ownedbooks", bytes.NewBufferString("isbn=9782416590316"))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 400, w.Code)
+	assert.Equal(t, "Invalid ISBN (9782416590316) provided.", w.Body.String())
 }
