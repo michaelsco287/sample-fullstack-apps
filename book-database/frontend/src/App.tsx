234b60419ef22b0894ab20a9f1949bed42277a0f
Nickson Thanda <nicksonthanda10@msn.com>
2020-02-10 12:20:07 +0000

    quick initial design

--- book-database/frontend/src/App.tsx
+++ book-database/frontend/src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="div1">        <img src={logo} className="App-logo" alt="logo" /></div>
+      <div className="div2"> 
+      <form>
+  <label>
+    Search for a book by its ISBN: 
+     <input type="text" name="isbn" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+</div>
+      <div className="div3">
+        <h1>Owned Books</h1>
+      </div>
+      <div className="div4">
+        <h1>Wanted Books</h1>
+        </div>  
     </div>
   );
 }
